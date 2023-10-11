b66c4fa28f7190d94d5e7a5edccb9b2d952e15a8
Sriharsha Kalluru <sriharsha@thinkdeeply.com>
2023-10-11 07:54:36 +0530

    Expense Scripts

--- 09-conditions/main.tf
+++ 09-conditions/main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "test" {
-  ami = "ami-03265a0778a880afb"
+  ami           = "ami-03265a0778a880afb"
   instance_type = var.instance_type == null ? "t3.medium" : var.instance_type
 }
 
