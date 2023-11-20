da3de66a395baeacab876d8d8e6ce657dbd25c3e
Sriharsha Kalluru <sriharsha@thinkdeeply.com>
2023-11-20 08:34:13 +0530

    [D75-7] | Raghu K | Terraform Code for RoboShop Project.

--- 06-loops/main.tf
+++ 06-loops/main.tf
@@ -5,3 +5,25 @@ resource "null_resource" "null" {
   }
 }
 
+variable "fruits" {
+  default = {
+    apple = {
+      name = "Apple"
+      count = 100
+    }
+
+    banana = {
+      name = "Banana"
+      count = 20
+    }
+
+  }
+}
+
+resource "null_resource" "test1" {
+  for_each = var.fruits
+  provisioner "local-exec" {
+    command = "echo Fruit Name - ${each.key["name"]}, Fruit Count - ${each.key["count"]}"
+  }
+}
+
